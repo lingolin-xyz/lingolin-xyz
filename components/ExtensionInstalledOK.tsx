00a518f550d2f9835226da37772000c4e27c1c8b
Javi <javierezpeleta@gmail.com>
2025-02-27 16:07:38 +0100

    not working yet...

--- components/ExtensionInstalledOK.tsx
+++ components/ExtensionInstalledOK.tsx
@@ -4,18 +4,33 @@ import { useEffect } from "react"
 import Title from "./Title"
 import { Button } from "./ui/button"
 
+import { usePrivy } from "@privy-io/react-auth"
+import LoginButton from "./LoginButton"
+
 const ExtensionInstalledOK = () => {
-  const sendMessage = () => {
-    window.postMessage(
-      { type: "LINGOLIN_MESSAGE", data: "Hello from Next.js!" },
-      "*"
-    )
-  }
+  const { user } = usePrivy()
+
+  useEffect(() => {
+    if (user)
+      window.postMessage(
+        { type: "LINGOLIN_USER_ID", data: "Hello from Next.js!" },
+        "*"
+      )
+    else {
+      window.postMessage({ type: "LINGOLIN_USER_ID", data: "-" }, "*")
+    }
+  }, [user])
+
+  const sendMessage = () => {}
 
   return (
     <div>
       <Title>ExtensionInstalledOK</Title>
-      <Button onClick={sendMessage}>Send Message</Button>
+      {user ? (
+        <Button onClick={sendMessage}>Send Message</Button>
+      ) : (
+        <LoginButton />
+      )}
     </div>
   )
 }
