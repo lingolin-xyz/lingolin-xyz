45ff35bd54e3b1d212045752d74b4a102ca3e6e3
Javi <javierezpeleta@gmail.com>
2025-02-28 22:03:31 +0100

    fix typos

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,13 +4,13 @@ const Footer = () => {
   return (
     <div className="border-t pt-4 space-y-2 text-xs">
       <div>
-        Lingolin as a project idea for the{" "}
+        Lingolin as a project for the{" "}
         <Link
           className="text-indigo-500 font-semibold hover:text-indigo-400 active:opacity-60 transition-all duration-100"
           href="https://hackathon.monad.xyz/"
           target="_blank"
         >
-          Monad evh/accathon
+          Monad evm/accathon
         </Link>
       </div>
       <div>
