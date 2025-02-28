2d374251404bc72256fa3aa21c9ac602c9e37f27
Javi <javierezpeleta@gmail.com>
2025-02-28 21:17:41 +0100

    header: silly detail for the logo on hover/click

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,20 +1,25 @@
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
+import { Button } from "@/components/ui/button"
+import Link from "next/link"
 
 const Header = () => {
   return (
     <div className="border-b pb-4  flex items-center justify-between">
       <Link href="/">
-        <Button variant="outline" className="font-bold text-sm">
+        <Button variant="outline" className="font-bold text-sm group">
           <div className="flex items-center gap-2">
-            <div className="hidden md:block">home</div>
-            <div className="block md:hidden">home</div>
+            <div className="">
+              <img
+                src="/images/logo.png"
+                className="h-6 group-hover:rotate-[360deg] transition-all duration-500 group-active:scale-[60%] group-active:rotate-[720deg] group-active:duration-300"
+              />
+            </div>
+            <div className="">home</div>
           </div>
         </Button>
       </Link>
       <div className="flex items-center gap-2">.</div>
     </div>
-  );
-};
+  )
+}
 
-export default Header;
+export default Header
