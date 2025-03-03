5c210e1da59f1ff6f98e718b21487d20cf79cc21
Javi <javierezpeleta@gmail.com>
2025-03-03 19:09:41 +0100

    adding button to tutorial (coming soon)

--- components/ExtensionInstalledOK.tsx
+++ components/ExtensionInstalledOK.tsx
@@ -4,9 +4,14 @@ import { useEffect } from "react"
 import { usePrivy } from "@privy-io/react-auth"
 
 import LoginScreen from "./LoginScreen"
+import BigTitle from "./BigTitle"
+import { Button } from "./ui/button"
+import { useToast } from "@/hooks/use-toast"
 
 const ExtensionInstalledOK = () => {
   const { user } = usePrivy()
+
+  const { toast } = useToast()
 
   useEffect(() => {
     if (user)
@@ -26,10 +31,21 @@ const ExtensionInstalledOK = () => {
     <div className="py-4">
       {user ? (
         <div>
-          <div className="flex flex-col gap-2">
-            <div>
+          <div className="flex flex-col gap-6 py-12 max-w-2xl mx-auto justify-center items-center">
+            <BigTitle>Welcome to Lingolin!</BigTitle>
+            <Button
+              onClick={() => {
+                toast({
+                  title: "Coming soon",
+                  description: "We're working on it!",
+                })
+              }}
+            >
+              Watch Tutorial Video
+            </Button>
+            {/* <div>
               <pre className="text-xs">{JSON.stringify(user, null, 2)}</pre>
-            </div>
+            </div> */}
           </div>
         </div>
       ) : (
