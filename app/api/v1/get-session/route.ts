89f90c80183de3d6ad01bd1669104d68bb493f10
Javi <javierezpeleta@gmail.com>
2025-03-03 19:15:23 +0100

    better logging...

--- app/api/v1/get-session/route.ts
+++ app/api/v1/get-session/route.ts
@@ -3,28 +3,34 @@ import { postErrorToDiscord } from "@/lib/discord"
 import { NextResponse } from "next/server"
 
 export async function POST(req: Request) {
-  const { userId } = await req.json()
+  try {
+    const { userId } = await req.json()
 
-  if (!userId) {
-    await postErrorToDiscord("------ no userId wtf!")
-    console.log("------ no userId wtf!")
+    if (!userId) {
+      await postErrorToDiscord("------ no userId wtf!")
+      console.log("------ no userId wtf!")
 
-    return NextResponse.json({ error: "userId is required" }, { status: 400 })
-  }
-
-  const { credits, tier } = await getUserAndCredits(userId)
-
-  return NextResponse.json(
-    {
-      credits,
-      tier,
-    },
-    {
-      headers: {
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
-        "Access-Control-Allow-Headers": "Content-Type, Authorization",
-      },
+      return NextResponse.json({ error: "userId is required" }, { status: 400 })
     }
-  )
+
+    const { credits, tier } = await getUserAndCredits(userId)
+
+    return NextResponse.json(
+      {
+        credits,
+        tier,
+      },
+      {
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+          "Access-Control-Allow-Headers": "Content-Type, Authorization",
+        },
+      }
+    )
+  } catch (error) {
+    await postErrorToDiscord("------ error on /api/v1/get-session!!")
+    console.log("------ error on /api/v1/get-session: " + error)
+    return NextResponse.json({ error: "error" }, { status: 500 })
+  }
 }
