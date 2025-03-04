6bf4ddc6120250b1d730ecdab3747217859930cc
Javi <javierezpeleta@gmail.com>
2025-03-04 19:43:50 +0100

    feat: added video demo + link to twitter on regular no-extension home page

--- components/NoExtensionFound.tsx
+++ components/NoExtensionFound.tsx
@@ -6,17 +6,14 @@ import BlurryEntrance from "./BlurryEntrance"
 import Link from "next/link"
 import Title from "./Title"
 import { useToast } from "@/hooks/use-toast"
+import { FaXTwitter } from "react-icons/fa6"
+import { Button } from "./ui/button"
 
 const NoExtensionFound = () => {
   const { toast } = useToast()
 
   return (
     <div className="py-10 max-w-7xl mx-auto w-full h-full px-2 text-center justify-center items-center flex flex-col gap-4">
-      <BigTitle>
-        <BlurryEntrance delay={0.3}>
-          <div className="py-12">Welcome to Lingolin</div>
-        </BlurryEntrance>
-      </BigTitle>
       <BlurryEntrance delay={0.5}>
         <div className="flex justify-center max-w-2xl items-center border-4 border-black rounded-3xl overflow-hidden">
           <motion.img
@@ -61,6 +58,44 @@ const NoExtensionFound = () => {
           </div>
         </div>
       </BlurryEntrance>
+      <div>
+        <BigTitle>
+          <BlurryEntrance delay={0.3}>
+            <div className="pt-6">Welcome to Lingolin</div>
+          </BlurryEntrance>
+        </BigTitle>
+        <div className="font-semibold text-xl opacity-70 pb-6">
+          A Chrome Extension for the Monad Hackathon
+        </div>
+        <BlurryEntrance delay={0.5}>
+          <div className="w-full flex justify-center items-center border-4 border-black max-w-2xl rounded-xl overflow-hidden">
+            <video
+              src="/videos/lingolin-demo-1.mp4"
+              controls
+              className="scale-[101%]"
+            />
+          </div>
+        </BlurryEntrance>
+        <BigTitle>
+          <BlurryEntrance delay={0.3}>
+            <div className="pt-24">Lingolin will be available soon!</div>
+          </BlurryEntrance>
+        </BigTitle>
+        <div className="font-semibold text-2xl opacity-70 py-6">
+          Come back for more updates!
+        </div>
+
+        <div>
+          <Link href="https://twitter.com/hellolingolin" target="_blank">
+            <Button variant="outline" className="group">
+              <div className="flex items-center gap-2">
+                <FaXTwitter className="w-6 h-6 -translate-y-[1.3px] group-hover:rotate-[360deg] transition-all duration-500 group-active:scale-[60%] group-active:rotate-[720deg] group-active:duration-300" />
+                <div>Follow us on Twitter</div>
+              </div>
+            </Button>
+          </Link>
+        </div>
+      </div>
     </div>
   )
 }
