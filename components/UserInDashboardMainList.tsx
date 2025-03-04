2a7c37e23be22afe0b257540239872c37848cfaa
Javi <javierezpeleta@gmail.com>
2025-03-05 00:14:05 +0100

    preparing interface for editing credits & practice nillion stuff here

--- components/UserInDashboardMainList.tsx
+++ components/UserInDashboardMainList.tsx
@@ -1,8 +1,35 @@
+"use client"
+
+import NumberFlow from "@number-flow/react"
+import { Button } from "./ui/button"
+import { Plus, Minus } from "lucide-react"
+import { useState } from "react"
+
 const UserInDashboardMainList = ({ user }: { user: any }) => {
+  const [credits, setCredits] = useState(user.credits)
+
+  const handleMinus = () => {
+    setCredits(credits - 1)
+  }
+
+  const handlePlus = () => {
+    setCredits(credits + 1)
+  }
+
   return (
     <div className="flex items-center justify-between bg-zinc-100 p-3 px-6 rounded-lg">
       <div>{user.email}</div>
-      <div>{user.credits}</div>
+      <div className="flex items-center gap-0">
+        <Button variant="outline" size="icon" onClick={handleMinus}>
+          <Minus size={12} />
+        </Button>
+        <div className="text-2xl tabular-nums font-bold tracking-tighter text-center w-16 translate-y-0.5">
+          <NumberFlow value={credits} />
+        </div>
+        <Button variant="outline" size="icon" onClick={handlePlus}>
+          <Plus size={18} />
+        </Button>
+      </div>
     </div>
   )
 }
