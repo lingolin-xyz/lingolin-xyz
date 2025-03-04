0c0a91c92ad3d45734398792d1288453550c43a8
Javi <javierezpeleta@gmail.com>
2025-03-04 07:48:43 +0100

    listing recent translations + making it look better

--- components/ExtensionInstalledOK.tsx
+++ components/ExtensionInstalledOK.tsx
@@ -47,7 +47,7 @@ const ExtensionInstalledOK = () => {
     <div className="py-4">
       {user ? (
         <div className="w-full">
-          <div className="flex flex-col gap-6 py-12 max-w-2xl mx-auto justify-center items-center hello w-full">
+          <div className="flex flex-col gap-6 py-12 max-w-2xl mx-auto justify-center items-center w-full">
             <BigTitle>Welcome to Lingolin!</BigTitle>
             <Button
               onClick={() => {
@@ -60,9 +60,9 @@ const ExtensionInstalledOK = () => {
               Watch Tutorial Video
             </Button>
 
-            <div>
-              <Title>Past Translations</Title>
-              <div className="flex flex-col gap-3 w-full max-w-xl mx-auto">
+            <div className="w-full max-w-2xl mx-auto bg-zinc-100 rounded-2xl p-6 md:px-8">
+              <Title>Recent Translations</Title>
+              <div className="flex flex-col gap-3 w-full mx-auto pt-2">
                 {pastTranslations.map(
                   (
                     translation: {
@@ -74,12 +74,21 @@ const ExtensionInstalledOK = () => {
                     },
                     index
                   ) => (
-                    <div key={index} className="hello p-2 rounded-lg">
-                      <div>
-                        <div>{translation.extra}</div>
-                        <div>{translation.extra3}</div>
-                        <div>{translation.extra2}</div>
-                        <div>{translation.extra4}</div>
+                    <div key={index} className="p-2 rounded-lg bg-white">
+                      <div className="space-y-2">
+                        <div className="relative text-base md:text-lg font-semibold opacity-70 tracking-tighter pt-4 bg-transparent hover:bg-yellow-100 rounded-xl px-2 cursor-pointer hover:opacity-90 active:opacity-40 group transition-all duration-300">
+                          <div className="absolute text-indigo-600 top-0 left-0 font-normal text-sm rotate-0 group-hover:-rotate-6 transition-all duration-100 group-hover:scale-125">
+                            from {translation.extra}
+                          </div>
+                          {translation.extra3}
+                        </div>
+                        <div className="relative text-xl md:text-2xl font-bold tracking-tight pt-5 bg-transparent group hover:bg-emerald-200 rounded-xl px-2 cursor-pointer hover:opacity-90 active:opacity-40 transition-all duration-300">
+                          <div className="absolute top-0 left-0 font-normal text-sm rotate-0 group-hover:-rotate-6 transition-all duration-100 group-hover:scale-125">
+                            to {translation.extra2}
+                          </div>
+
+                          {translation.extra4}
+                        </div>
                       </div>
                     </div>
                   )
