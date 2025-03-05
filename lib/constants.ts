b613aa5fd13216634c742a252a13f2545ecc2bcc
Javi <javierezpeleta@gmail.com>
2025-03-05 10:34:40 +0100

    adding han.test as an admin, to access the dashboard

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,4 +1,4 @@
 export const IS_LOCALHOST = process.env.NODE_ENV === "development"
 
 export const PRIVY_APP_ID = "cm7kput8203rjqfjfh7qhsnj6"
-export const ADMIN_EMAILS = ["hellolingolin@gmail.com"]
+export const ADMIN_EMAILS = ["hellolingolin@gmail.com", "han.test@fbrns.co"]
