7aabf308369e3a1de83345f5df9aa15ca9571f08
Javi <javierezpeleta@gmail.com>
2025-03-05 13:17:08 +0100

    wip linking wallets! 😓

--- components/wallet/UserWalletClientExplorer.tsx
+++ components/wallet/UserWalletClientExplorer.tsx
@@ -1,8 +1,10 @@
 "use client"
 
+import { useLinkAccount, usePrivy } from "@privy-io/react-auth"
 import { useUser } from "@privy-io/react-auth"
 import React from "react"
 import { useAccount } from "wagmi"
+import { Button } from "../ui/button"
 
 interface UserWalletClientExplorerProps {
   userId?: string
@@ -11,17 +13,35 @@ interface UserWalletClientExplorerProps {
 const UserWalletClientExplorer: React.FC<UserWalletClientExplorerProps> = ({
   userId,
 }) => {
+  //   const { linkWallet } = usePrivy()
   const { address } = useAccount()
   const { user } = useUser()
+
+  const { linkWallet } = useLinkAccount({
+    onSuccess: ({ user, linkMethod, linkedAccount }) => {
+      console.log(user, linkMethod, linkedAccount)
+      // Any logic you'd like to execute if the user successfully links an account while this
+      // component is mounted
+    },
+    onError: (error, details) => {
+      console.log(error, details)
+      // Any logic you'd like to execute after a user exits the link flow or there is an error
+    },
+  })
+
   return (
     <div className="rounded-md border p-4 mt-4">
       <h3 className="text-lg font-medium mb-2">Wallet Explorer</h3>
       <p className="text-sm text-gray-500">
         User ID: {user?.id || "Not available"}
       </p>
+      {/* {!address ? ( */}
+      <Button onClick={linkWallet}>Link your wallet</Button>
+      {/* ) : ( */}
       <p className="text-sm text-gray-500">
         Wallet Address: {address || "Not available"}
       </p>
+      {/* )} */}
     </div>
   )
 }
