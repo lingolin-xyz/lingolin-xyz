84e5499f7d213bae5ed468fd658296db264fc58a
Javi <javierezpeleta@gmail.com>
2025-03-05 10:54:24 +0100

    fix: finding the email properly now (while limiting access to the dashboard)

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,9 +1,10 @@
 import { cookies } from "next/headers"
 import { PrivyClient } from "@privy-io/server-auth"
 import { ADMIN_EMAILS, PRIVY_APP_ID } from "@/lib/constants"
-import { postToDiscord } from "@/lib/discord"
 import DashboardHeaderBar from "@/components/DashboardHeaderBar"
 import { getEmailAddressFromPrivyUserObject } from "@/lib/privy-utils-client"
+import Link from "next/link"
+import { Button } from "@/components/ui/button"
 
 const DashboardLayout = async ({ children }: { children: React.ReactNode }) => {
   const cookieStore = await cookies()
@@ -25,17 +26,23 @@ const DashboardLayout = async ({ children }: { children: React.ReactNode }) => {
     return <div>No user found :( </div>
   }
 
-  if (!user.email) {
+  const email = getEmailAddressFromPrivyUserObject(user)
+  if (!email) {
     return <div>No email found</div>
   }
-
-  const email = getEmailAddressFromPrivyUserObject(user)
   if (!email) {
     return <div>No email found (v2)</div>
   }
 
   if (!ADMIN_EMAILS.includes(email)) {
-    return <div>You are not authorized to access this page</div>
+    return (
+      <div className="p-4 w-full flex items-center justify-center flex-col gap-4">
+        <div>You are not authorized to access this page</div>
+        <Link href="/">
+          <Button variant="outline">Go back to home</Button>
+        </Link>
+      </div>
+    )
   }
 
   return (
