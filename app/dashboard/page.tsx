47cd2db3f6665c4a5b907b6cf2380a3217a63f30
Javi <javierezpeleta@gmail.com>
2025-03-05 10:39:18 +0100

    tiny tweak just to see if dev branch goes live on the secondary vercel domain thingy

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -7,14 +7,10 @@ const DashboardPage = async () => {
   // Usar el método getUser del cliente
 
   return (
-    <div className="bg-orange-200 p-12 rounded-3xl mt-12 flex flex-col gap-3 items-center justify-center">
+    <div className="bg-orange-100 p-12 rounded-xl mt-12 flex flex-col gap-3 items-center justify-center">
       <Title>Dashboard</Title>
 
-      <div className="flex flex-col gap-3 w-60 mx-auto">
-        <Link href="dashboard/translations">
-          <Button>Translations</Button>
-        </Link>
-      </div>
+      <div>Dashboard home is coming soon</div>
     </div>
   )
 }
