d3a19a4047952eb472d9d25449e3b31bed71b99b
Javi <javierezpeleta@gmail.com>
2025-03-12 12:59:20 +0100

    redirecting to the music album preview

---
+++ app/music/page.tsx
@@ -0,0 +1,13 @@
+"use client"
+
+import { useEffect } from "react"
+
+const MusicPage = () => {
+  useEffect(() => {
+    window.location.href =
+      "https://distrokid.com/hyperfollow/0xjavi/lingolin-monad-hackathon-march-2025"
+  }, [])
+  return <div></div>
+}
+
+export default MusicPage
